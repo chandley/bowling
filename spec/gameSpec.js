8e375f8d277db71a4096f1a6d237f28f2c7009b3
Claudia <claudia.donovan89@gmail.com>
2014-11-28 18:10:39 +0000

    test for game to calculate total score - failing

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,9 +1,11 @@
 describe('Game',function(){
     
   var game;
+  var frame;
   
   beforeEach(function(){
     game = new Game();
+    frame = new Frame();
   });
     
   describe('on setup', function(){
@@ -12,6 +14,16 @@ describe('Game',function(){
       expect(game.frames.length).not.toEqual(0);
     });
         
+  });
+
+  describe('when scoring game', function(){
+
+  	it('can give a total score from a frame without bonus', function(){
+  		game.frames[0].playARoll(3)
+
+  		expect(game.totalScore).toEqual(3)
+  	});
+
   });    
     
 });
