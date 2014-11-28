78d0587bab27cb6606d0e9128db3e23a33538c70
Claudia <claudia.donovan89@gmail.com>
2014-11-28 14:15:53 +0000

    frame has rolls array of undefined elements - to be defined

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -8,9 +8,24 @@ describe('Frame', function() {
 
   describe('frame setup', function(){
 
-    it('should contain a pin', function() {
-      expect(frame.pins.length).not.toEqual(0);
-    })
-  })
+    it('should contain at least one roll', function() {
+      expect(frame.rolls.length).not.toEqual(0);
+    });
+
+    it('the roll should not contain a value until played', function(){
+    	expect(frame.rolls[0]).not.toBeDefined();
+    	expect(frame.rolls[1]).not.toBeDefined();
+    });
+
+  });
+
+  describe('recording a roll', function(){
+
+  	it('can have value after being played', function(){
+  		frame.playARoll();
+  		expect(frame.rolls[0]).toEqual(5);
+  	});
+
+  });
 
 });
