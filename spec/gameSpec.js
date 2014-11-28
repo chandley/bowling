d35218a051903cb5fc3c62aef7e9ba1a8be9742c
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-28 18:20:47 +0000

    failing test game score two frames with strike

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -20,9 +20,20 @@ describe('Game',function(){
 
   	it('can give a total score from a frame without bonus', function(){
   		game.frames[0].playARoll(3)
-
-  		expect(game.totalScore).toEqual(3)
+  		expect(game.totalScore()).toEqual(3)
   	});
+
+    it('can give another total score from a frame without bonus', function(){
+      game.frames[0].playARoll(6)
+      expect(game.totalScore()).toEqual(6)
+    });
+
+    it('can give a score from two frames with a strike first frame', function() {
+      game.frames[1].playARoll(5);
+      game.frames[1].playARoll(1);
+      game.frames[0].playARoll(10);
+      expect(game.frames.totalScore).toEqual(22);
+    });
 
   });    
     
