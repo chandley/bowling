391ac16b132e3391aeced6263c7139c3be178777
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-01 15:35:52 +0000

    final tidy up - spaces, redundant brackets, test names

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -18,17 +18,17 @@ describe('Game',function(){
 
   describe('when scoring game', function(){
 
-  	it('can give a total score from a frame without bonus', function(){
+  	it('can give a total score from a single roll of 3', function(){
   		game.frames[0].playARoll(3)
   		expect(game.totalScore()).toEqual(3)
   	});
 
-    it('can give another total score from a frame without bonus', function(){
+    it('can give a total score from a single roll of 6', function(){
       game.frames[0].playARoll(6)
       expect(game.totalScore()).toEqual(6)
     });
 
-    it('can give a score from two games no bonus', function() {
+    it('can score 2 frames no bonus (roll 2,1 roll 4,2)', function() {
       game.frames[0].playARoll(2);
       game.frames[0].playARoll(1);     
       game.frames[1].playARoll(4);
@@ -36,7 +36,7 @@ describe('Game',function(){
       expect(game.totalScore()).toEqual(9);
     });
 
-    it('can give a score from two frames with a strike first frame', function() {
+    it('can score a strike followed by second frame (roll 10, roll 5,1)', function() {
 			game.frames[0].playARoll(10);     
       game.frames[1].playARoll(5);
       game.frames[1].playARoll(1);
@@ -44,7 +44,7 @@ describe('Game',function(){
       expect(game.totalScore()).toEqual(22);
     });
 
-    it('can give a score from three strikes in a row', function() {
+    it('can score from three strikes in a row', function() {
       game.frames[0].playARoll(10);     
       game.frames[1].playARoll(10);
       game.frames[2].playARoll(10);
@@ -53,7 +53,7 @@ describe('Game',function(){
     });
 
 
-    it('can give a score from multiple frames', function(){
+    it('can score from multiple frames', function(){
     	game.frames[0].playARoll(1);  
     	game.frames[0].playARoll(4);   
       game.frames[1].playARoll(4);
@@ -93,7 +93,7 @@ describe('Game',function(){
         expect(game.totalScore()).toEqual(150);
     });
 
-    it('can give a score from full example game', function(){
+    it('can score from full example game', function(){
       game.frames[0].playARoll(1);  
       game.frames[0].playARoll(4);   
       game.frames[1].playARoll(4);
