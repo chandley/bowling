b0bf9330b3282a9145e7d5410bef53400dcb8efc
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-01 09:22:54 +0000

    refactor frame with roll. frame tests passing

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -12,11 +12,6 @@ describe('Frame', function() {
       expect(frame.rolls.length).not.toEqual(0);
     });
 
-    it('the roll should not contain a value until played', function(){
-    	expect(frame.rolls[0]).not.toBeDefined();
-    	expect(frame.rolls[1]).not.toBeDefined();
-    });
-
     it('is not played when created', function() {
       expect(frame.isPlayed()).toEqual(false);
     });
@@ -32,7 +27,7 @@ describe('Frame', function() {
 
   	it('can have value after being played', function(){
   		frame.playARoll(5);
-  		expect(frame.rolls[0]).toEqual(5);
+  		expect(frame.rolls[0].pinsHit).toEqual(5);
   	});
 
     it('knows when it has been played', function() {
@@ -43,15 +38,15 @@ describe('Frame', function() {
     it('can record two rolls', function(){
       frame.playARoll(5);
       frame.playARoll(3);
-      expect(frame.rolls[0]).toEqual(5);
-      expect(frame.rolls[1]).toEqual(3);
+      expect(frame.rolls[0].pinsHit).toEqual(5);
+      expect(frame.rolls[1].pinsHit).toEqual(3);
     });
 
   it('can record another two rolls', function(){
       frame.playARoll(3);
       frame.playARoll(7);
-      expect(frame.rolls[0]).toEqual(3);
-      expect(frame.rolls[1]).toEqual(7);
+      expect(frame.rolls[0].pinsHit).toEqual(3);
+      expect(frame.rolls[1].pinsHit).toEqual(7);
     });
 
   });
